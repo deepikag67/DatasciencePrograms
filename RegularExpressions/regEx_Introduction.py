115e78d556ad8d21a6ee5107625ef7c840de5341
deepikag67 <deepikatech0@gmail.com>
2023-06-14 00:03:39 +0800

    committing regEx files

---
+++ RegularExpressions/regEx_Introduction.py
@@ -0,0 +1,26 @@
+# Regular Expression(RegEx/Regexes)
+# Identify the sequence of characters in a pattern
+# Matches Strings
+
+# Identify the character index for a string
+f = open("C://Technology//DevOps//Steps//gitcommands.txt", "r")
+s = "Deepika@DESKTOP-V34C1IL"
+a = s.find("3")
+print(a)
+
+
+# Slicing the string
+f = open("C://Technology//DevOps//Steps//gitcommands.txt", "r")
+s = "Deepika@DESKTOP-V34C1IL"
+a = s[0:4]
+print(a)
+
+
+# Metacharacters
+# Special sequences
+# sets
+
+
+
+
+
--- RegularExpressions/regEx_metaCharacters.py
+++ RegularExpressions/regEx_metaCharacters.py
@@ -1,4 +1,27 @@
-# Back Slash "\"
+# Metacharacters - The syntax contains special characters - [],\,.,^,$,*,(),|,{},+,?
+# Each special character identifies the string in different ways.
+
+
+# Using [] - Square brackets - Return the lower case characters between a and l
+import re
+
+s = "Deepika@DESKTOP-V34C1IL"
+f = re.findall("[a-l]", s) # lower case
+print(f)
+
+
+# Using \ - Back Slash - Return the digits/numbers in the string
+s = "Deepika@DESKTOP-V34C1IL7"
+f = re.findall("\d", s)
+print(f)
+
+
+# Using . - dot - Return a specific sequence in a string
+s = "Deepika"
+f = re.findall("De.a", s)
+print(f)
+
+
 
 
 
