57661f8f140904f01bbdd0deb2b7ff98305c852f
deepikag67 <deepikatech0@gmail.com>
2023-06-21 22:42:01 +0800

    committing changes

--- RegularExpressions/metacharacters/regEx_metaCharacters_dot.py
+++ RegularExpressions/metacharacters/regEx_metaCharacters_dot.py
@@ -11,20 +11,20 @@ g = "She is best for me. I love her endlessly. I cant live without her. Because 
 d = re.findall(r"\.", g)
 print(d)
 
-<<<<<<< HEAD
+
 "This is a comment"
 'This is a comment'
 '''This is a comment'''
 """ This is a text """
 
 print("Hello World")
-=======
+
 import re
 a = "Vivo phones. are#good.@but-iphone are.very\good."
 d = re.findall(r"\.,", a)
 print(d)
 
 
->>>>>>> a3f9e54595583e5a971c63da0bfdfbcf2f734547
+
 
 # This is a comment
