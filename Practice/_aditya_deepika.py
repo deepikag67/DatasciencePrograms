93768658f69fecc556aec9326a7badb1e5c77194
deepikag67 <deepikatech0@gmail.com>
2023-06-21 15:03:02 +0530

    committing latest changes\

--- Practice/_aditya_deepika.py
+++ Practice/_aditya_deepika.py
@@ -25,7 +25,34 @@ print('\u2764')
 print('\u2764')
 
 
+import math
+import turtle
 
+turtle.shape("turtle")
+turtle.setup(width=900, height=700)
+turtle.speed(0)
+turtle.bgcolor('black')
+turtle.color('red')
+
+def heart1(h):
+    return 15 * math.sin(h) ** 3
+
+def heart2(h):
+    return 12 * math.cos(h) - 5 * math.cos(2 * h) - 2 * math.cos(3 * h) - math.cos(4 * h)
+
+
+
+for i in range(600):
+    x, y = heart1(i) * 20, heart2(i) * 20
+    turtle.goto(x, y)
+
+# Draw "I Love You" in the center
+turtle.penup()
+turtle.goto(0, -45)
+turtle.pendown()
+turtle.write('I Love You', align='center', font=('Arial', 50, 'bold'))
+
+turtle.done()
 
 
 
--- RegularExpressions/sets/sets_introduction.py
+++ RegularExpressions/sets/sets_introduction.py
@@ -7,7 +7,7 @@ import re
 # Returns alphanumeric
 # Return all occurrences
 s = "IDI/08_AI-(5449)-PR98042"
-f = re.findall("[5ID]", s)
+f = re.findall("[8ID]", s)
 print(f)
 
 
@@ -57,4 +57,59 @@ print(g)
 print(h)
 
 
+import re
+# Return the match of the specified characters inside the [].
+# Ex: Pythonn11 - [yon1] - returns [y,o,n,n,1,1]
+# Returns alphanumeric
+# Return all occurrences
+s = "24/01/1997Aditya@loves31/01/1988Deepika"
+d = re.findall("[01AD]", s)
+print(d)
 
+# Return a match for any lower case character only
+# Does not return Upper case character
+s = "24/01/1997Aditya@loves31/01/1988Deepika"
+d = re.findall("[a-z]", s)
+print(d)
+
+# Return a match for any lower case character only
+# Does not return lower case character
+s = "24/01/1997Aditya@loves31/01/1988Deepika"
+d = re.findall("[A-Z]", s)
+print(d)
+
+# Does not return match for characters mentioned in square brackets
+s = "24/01/1997Aditya@loves31/01/1988Deepika"
+d = re.findall("[^i]", s)
+print(d)
+
+# Returns the match for the specified digits in the string
+# Returns all the occurrences
+s = "24/01/1997Aditya@loves31/01/1988Deepika"
+d = re.findall("[09]", s)
+print(d)
+
+
+# Returns a match for any digit between 0 and 9
+s = "24/01/1997Aditya@loves31/01/1988Deepika"
+d = re.findall("[0-9]", s)
+print(d)
+
+# Returns a match between any two digit numbers 00 and 59
+s = "24/01/1997Aditya@loves31/01/1988Deepika"
+d = re.findall("[0-5] [0-9]", s)
+print(d)
+
+# Returns a match for any character alphabetically between a and z
+s = "24/01/1997Aditya@loves31/01/1988Deepika"
+d = re.findall("[a-zA-Z]", s)
+print(d)
+
+# Returns a match for a string with any special character character
+s = "24/01/1997$Aditya@love+s31/01/1988De*epika"
+d = re.findall("[$]", s)
+g = re.findall("[+]", s)
+h = re.findall("[*]", s)
+print(d)
+print(g)
+print(h)
