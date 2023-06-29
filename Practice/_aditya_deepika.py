5fbf6f3d7628ff88bc139b1ad14c95211634a85a
deepikag67 <deepikatech0@gmail.com>
2023-06-29 14:44:04 +0530

    committing latest changes

--- Practice/_aditya_deepika.py
+++ Practice/_aditya_deepika.py
@@ -25,6 +25,63 @@ print('\u2764')
 print('\u2764')
 
 
+import turtle
+
+# Set up the turtle screen
+screen = turtle.Screen()
+screen.title("Happy Journey!")
+screen.bgcolor("white")
+screen.setup(width=800, height=600)
+
+# Create the turtle object
+heart = turtle.Turtle()
+heart.shape("turtle")
+heart.color("red")
+heart.speed(2)
+
+# Move the turtle to the starting position
+heart.penup()
+heart.goto(0, -200)
+heart.pendown()
+
+# Draw the heart shape
+heart.begin_fill()
+heart.left(140)
+heart.forward(224)
+for _ in range(200):
+    heart.right(1)
+    heart.forward(2)
+heart.left(120)
+for _ in range(200):
+    heart.right(1)
+    heart.forward(2)
+heart.forward(224)
+heart.end_fill()
+
+# Move the turtle to the text position
+heart.penup()
+heart.goto(0, 0)
+heart.pendown()
+
+# Write the text message
+heart.color("black")
+heart.write("Happy Journey Baby i pray ur all wish come true in this trip!", align="center", font=("Arial", 10, "bold"))
+
+# Hide the turtle
+heart.hideturtle()
+
+# Exit on click
+turtle.done()
+
+
+
+
+
+
+
+
+
+
 
 
 
