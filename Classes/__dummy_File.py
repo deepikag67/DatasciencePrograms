31287e1f70c7e4a04ef5b0366575056bb88036b8
deepikag67 <deepikatech0@gmail.com>
2023-07-11 16:05:06 +0530

    committing latest changes

--- Classes/__dummy_File.py
+++ Classes/__dummy_File.py
@@ -14,3 +14,58 @@ class employeeDetails :
 
 
 # Dummy Line
+
+x = 20
+print(x)
+print(type(x))
+
+x = 31.24
+print(x)
+print(type(x))
+
+x = 1j
+print(x)
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(x)
+print(type(x))
+
+x = range(6)
+print(x)
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+print(x)
+print(type(x))
+
+x = {"apple", "banana", "cherry"}
+print(x)
+print(type(x))
+
+
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x))
+
+
+x = b"Deepika"
+print(x)
+print(type(x))
+
+
+x = bytearray(6)
+print(x)
+print(type(x))
+
+x = memoryview(bytes(7))
+print(x)
+print(type(x))
+
+x = None
+print(x)
+print(type(x))
